6e81f51982d372ceb9ca0125edb6b0568964bc02
Brian Ali <gamerfunky78@gmail.com>
2024-06-10 18:19:20 +0300

    Add files via upload
    A simple calculator for js practice

---
+++ index.js
@@ -0,0 +1,42 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    
+    display.value += input;
+
+
+}
+function clearDisplay(){
+
+    display.value = "";
+
+
+
+}
+function calculate(){
+
+    try{  
+
+        display.value = eval(display.value);
+
+    }
+    catch(e){
+
+        display.value += "Error";
+
+
+    }
+    
+
+
+
+
+}
+function square(){
+    
+    display.value = display.value ** 2
+
+
+
+
+}
